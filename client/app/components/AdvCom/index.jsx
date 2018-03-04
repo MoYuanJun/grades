87b0fc240d58e9566b1ad8ced6a5fd88684528ac
qianyin925 <884820152@qq.com>
2018-03-04 09:31:03 +0800

    jsx文件添加布局

---
+++ client/app/components/AdvCom/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.less';
+class AdvCom extends React.Component{
+    render(){
+        return (
+            <div id="AdvCom">
+                <h2>店家推荐</h2>
+            </div>
+        );
+    }
+}
+export default AdvCom;
--- client/app/components/UserShow/index.jsx
+++ client/app/components/UserShow/index.jsx
@@ -4,33 +4,39 @@ import React from 'react';
 import './style.less';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import AdvCom from '../AdvCom';
 
 class UserShowComponent extends React.Component{
   render(){
     return (
-      <div id='userShow'>
-        <div className='top'>
-          <div className='headPortrait'> {/* 头像 */}
-            <img src={require('../../static/img/loginbg.png')} alt=""/>
+      <div id='brnach-right'>
+        <div className='userShow'>
+          <div className='top'>
+            <div className='headPortrait'> {/* 头像 */}
+              <img src={require('../../static/img/loginbg.png')} alt=""/>
+            </div>
+            <div className='welcome'>
+              Hi~{this.props.userInfo.u_id ? <span>{this.props.userInfo.username}</span> : 
+              <span>欢迎来到精品时装！</span>}
+            </div>
           </div>
-          <div className='welcome'>
-            Hi~{this.props.userInfo.u_id ? <span>{this.props.userInfo.username}</span> : 
-            <span>欢迎来到精品时装！</span>}
+          <div className='bottom'>
+            <div className='loginAndRegister clearfix'>
+              {this.props.userInfo.u_id ? 
+              <div>
+                <div className='login float-left'><Link to='/login/login'>退出登录</Link></div>
+                <div className='register float-right'><Link to={`/prospects/userHome/${this.props.userInfo.u_id}`}>个人中心</Link></div>
+              </div> :
+              <div>
+                <div className='login float-left'><Link to='/login/login'>登录入口</Link></div>
+                <div className='register float-right'><Link to='/login/register'>注册入口</Link></div>
+              </div>}
+            </div>
+            <div></div>
           </div>
         </div>
-        <div className='bottom'>
-          <div className='loginAndRegister clearfix'>
-            {this.props.userInfo.u_id ? 
-            <div>
-              <div className='login float-left'><Link to='/login/login'>退出登录</Link></div>
-              <div className='register float-right'><Link to={`/prospects/userHome/${this.props.userInfo.u_id}`}>个人中心</Link></div>
-            </div> :
-            <div>
-              <div className='login float-left'><Link to='/login/login'>登录入口</Link></div>
-              <div className='register float-right'><Link to='/login/register'>注册入口</Link></div>
-            </div>}
-          </div>
-          <div></div>
+        <div id="advertisingcommodity">
+          <AdvCom />
         </div>
       </div>
     );
